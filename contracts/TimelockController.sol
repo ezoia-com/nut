4964de5ab2f08656dd1344b0d9d1717e62db7634
ezoia-com <ezoia@ezoia.com>
2023-11-06 18:05:19 +0800

    Fix for coverage

--- contracts/TimelockController.sol
+++ contracts/TimelockController.sol
@@ -1,13 +1,4 @@
 // SPDX-License-Identifier: None
 pragma solidity ^0.8.19;
 
-import {TimelockController as OZTimelockController} from "../node_modules/@openzeppelin/contracts/governance/TimelockController.sol";
-
-contract Timelock is OZTimelockController {
-    constructor(
-        uint256 minDelay,
-        address[] memory proposers,
-        address[] memory executors,
-        address admin
-    ) OZTimelockController(minDelay, proposers, executors, admin) {}
-}
+import {TimelockController} from "../node_modules/@openzeppelin/contracts/governance/TimelockController.sol";
